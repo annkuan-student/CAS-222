fd32f0e7a2ab54f3320dd29a11896d22e237264f
annkuan-student <78815301+annkuan-student@users.noreply.github.com>
2021-03-09 23:32:56 -0800

    Add files via upload

---
+++ index.html.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="zxx">
+   <head>
+      <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
+      <title>Ace Events Template</title>
+      <meta charset="utf-8">
+      <link href="style/stylereset.css" rel="stylesheet">
+      <link href="style/style.less" rel="stylesheet/less" type="text/css"> 
+       <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.1/less.min.js"></script> 
+    </head>
+   <body>
+       <div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v10.0" nonce="8n6rYIIO"></script>
+      <header>
+          <span class="background-img" role="img" aria-label="hero"></span>
+          <a href="index.php"><img src="images/logo_white.png" alt="AceLogo"></a>
+        <?php include 'includes/nav.inc.html.php'; ?>
+          <button class="hamburger">&#9776;</button>
+         <button class="cross">&#735;</button> 
+      </header>
+      <main>
+         <div class="about">
+             <p>Ace in the Hole Multisport Events is proud to offer running and triathlon events to athletes of all shapes and sizes, national origins, gender identifications and cultural backgrounds. We offer Events for Every Body.</p>
+            </div>
+          
+         <div class="upcomingevents">
+             <h1>Upcoming Events</h1>
+             <p>The Annual <b>Ace in the Hole Multisport Weekend</b> is a legendary event in the Oregon triathlon and running community that takes place March 27th and 28th, 2021. It has become a traditional destination race for athletes from across the nation.</p></div>
+             <div class="collage" role="img" aria-label="collage"></div>
+          <div class="registerbutton"><button><a href="event/index.php">Learn more about the event!</a></button></div>
+         
+          <div class="socialfeeds">
+            <h1>What Are We Up To?</h1>  
+          <div class="fb">
+              <div class="fb-page" data-href="https://www.facebook.com/Cas222Aceinthehole-110661963841617/" data-tabs="timeline" data-width="500" data-height="500" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Cas222Aceinthehole-110661963841617/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Cas222Aceinthehole-110661963841617/">Cas222Aceinthehole</a></blockquote></div></div>
+         <div class="twitter">
+             <a class="twitter-timeline" data-width="500" data-height="500" data-theme="dark" href="https://twitter.com/pcccas222?ref_src=twsrc%5Etfw">Tweets by pcccas222</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></div>
+         </div>
+          
+      </main>
+      <?php include 'includes/footer.inc.html.php'; ?>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <script src="style/style.js"></script>
+   </body>
+</html>
